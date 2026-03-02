dfe8c01c3a140c03a5c82c6778bebca88d01fe7a
mallconx14 <Mallconx14@gmail.com>
2026-03-02 10:35:34 -0300

    ca

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const express = require("express");
 const app = express();
 
+app.get("/verstappen", (req, res, next) => {
+    return res.status(200).sendFile("verstappenn.html", {root: './'});
+});
