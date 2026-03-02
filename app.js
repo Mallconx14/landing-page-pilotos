419fc956cbdb10dfb005ebef0a422fbe06e1b2c2
mallconx14 <Mallconx14@gmail.com>
2026-03-02 10:45:18 -0300

    add module export

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/verstappen", (req, res, next) => {
-    return res.status(200).sendFile("verstappenn.html", {root: './'});
+    return res.status(200).sendFile("verstappen.html", {root: './'});
 });
+
+module.exports = app;
