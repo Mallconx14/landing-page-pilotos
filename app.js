08206a5e35042e6174c3d194ff3d01df1fda1e4c
dieggo7 <diegaomoura962@gmail.com>
2026-03-02 10:41:38 -0300

    branch feita

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.get("/bortoleto", (req, res, next) => {
+    return res.status(200).sendFile("bortoleto.html", {root: './'});
+});
+
+module.exports = app;
